ad1e81c415427492ab8b2dbb4e5aca43aa1ca1c3
Max Ehnert <max.ehnert@gmail.com>
2016-05-21 18:32:10 -0500

    Cleaning up styles and added more modal functionality.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,26 @@
 $('#buyNowModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
   var recipient = button.data('product'); // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this);
-  modal.find('.modal-sub-title').text('Add To Cart: ' + recipient);
+  var productType = button.data('bottle');
+  var price = button.data('price');
+
+  $('.modal-inner-content').css('display', 'initial');
+  $('.giphy-embed').css('display', 'none');
+
+  $(this).find('.modal-sub-title').text('Add To Cart: ' + recipient);
+  $(this).find('.price-input')[0].value = price;
+  $('.cap-color-container').css('display', 'initial');
+
+  if (!productType) {
+    $('.cap-color-container').css('display', 'none');
+  }
+});
+
+$('#icheckout-quantity').click(function(e) {
+  console.log(e);
+  console.log(this);
+
+  $('.price-input')[0].value = (($('.price-input').val() / ($(this).val() - 1)) * $(this).val());
 });
 
 $('.checkout').click(function(e) {
